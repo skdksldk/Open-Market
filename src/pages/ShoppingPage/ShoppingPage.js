8557be7b5447797e795b4ce5ac24d8e5bcc37cc1
skdksldk <tnwns1485@naver.com>
2022-01-22 01:11:14 +0900

    add item and cart function

--- src/pages/ShoppingPage/ShoppingPage.js
+++ src/pages/ShoppingPage/ShoppingPage.js
@@ -7,13 +7,13 @@ import ShoppingItem from "./Sections/ShoppingItem";
 function ShoppingPage() {
     const navigate = useNavigate();
     const [cart, setCart] = useState({});
-
-    useEffect(() => {
-        var Cart = clayful.Cart;
+    var Cart = clayful.Cart;
 
         var options = {
             customer: localStorage.getItem("accessToken"),
         };
+    
+    useEffect(() => {
 
         Cart.getForMe({}, options, function(err, result) {
 
@@ -23,25 +23,90 @@ function ShoppingPage() {
                 return;
             }
             var data = result.data;
+            console.log("data", data);
             setCart(data.cart) 
       });
     }, []);
 
+    const updateItemData = (itemId, quantity) => {
+        let payload = {
+          quantity,
+        };
+
+        Cart.updateItemForMe(itemId, payload, options, function (err, result) {
+          if (err) {
+            // Error case
+            console.log(err.code);
+            return;
+          }
+        });
+      };
+
+      const buttonHandler = (type, index) => {
+        let newCart = { ...cart };
+
+        if (type === "plus") {
+
+        const price = cart.items[index].price.original.raw / cart.items[index].quantity.raw;
+          // 해당 아이템 가격 변경
+          newCart.items[index].price.original.raw += price;
+          // 전체 아이템 가격 변경
+          newCart.total.amount.raw += price;
+          // 해당 아이템 갯수 변경
+          newCart.items[index].quantity.raw += 1;
+
+        } else {
+          if (newCart.items[index].quantity.raw === 1) return;
+
+          const price = cart.items[index].price.original.raw / cart.items[index].quantity.raw;
+          newCart.total.amount.raw -= price ;
+          newCart.items[index].price.original.raw -= price;
+          newCart.items[index].quantity.raw -= 1;
+        }
+        updateItemData(newCart.items[index]._id, newCart.items[index].quantity.raw);
+        setCart(newCart);
+      };
+
+    const removeItemFromState = (itemId, price) => {
+        let newCart = {...cart};
+        let filteredItems = newCart.items.filter(item => item._id !== itemId);
+        newCart.items = filteredItems;
+        newCart.total.amount.raw = newCart.total.amount.raw - price;
+        setCart(newCart);
+    }
+
+      const deleteItemHandler = (itemId, price) => {
+        Cart.deleteItemForMe(itemId, options, function(err, result) {
+
+            if (err) {
+                // Error case
+                console.log(err.code);
+                return;
+            }
+            removeItemFromState(itemId, price)
+
+        });
+      }
+
     const items = cart.items;
-    console.log('items',items);
-  return (
-    <div className="pageWrapper">
+    
+    return (
+      <div className="pageWrapper">
         <div className="shopping-cart">
             <h1 className="title">장바구니</h1>
 
             <div className="shopping-cart-body" style={{ minHeight:100}}> 
             {items && items.length > 0 ? (
                     items.map((item, index) => {
-                        return <ShoppingItem 
+                        return(
+                        <ShoppingItem 
                             key= {item._id}
                             item={item}
                             index={index}
-                        />;
+                            deleteItemHandler={(itemId, price) => deleteItemHandler(itemId, price)}
+                            buttonHandler={(type, index) => buttonHandler(type, index)}
+                        />
+                      );
                     })
                 ): (
                   <p style={{ textAlign: "center", marginTop: "2rem"}}>
