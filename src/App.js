54e3276527fc2a8e84ce39401f6816b2ae35707c
skdksldk <tnwns1485@naver.com>
2022-01-21 15:43:40 +0900

    maintain login

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import LandingPage from "./pages/LandingPage/LandingPage";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
+import AuthContextProvider from './context/AuthContext';
+
 
 function App() {
   return (
-    <Routes>
-     <Route path="/" element = {<LandingPage />} />
-     <Route path="/login" element = {<LoginPage />} />
-     <Route path="/register" element = {<RegisterPage />} />
-    </Routes>
+    <AuthContextProvider>
+     <Routes>
+      <Route path="/" element = {<LandingPage />} />
+      <Route path="/login" element = {<LoginPage />} />
+      <Route path="/register" element = {<RegisterPage />} />
+     </Routes>
+    </AuthContextProvider>
   );
 }
 
